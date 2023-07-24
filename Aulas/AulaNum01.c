1eba124f497955678c523af3995a2fa1be3777c5
unknown <pereira.rodrigo99@gmail.com>
2023-07-24 10:06:49 -0300

    aula 01

---
+++ Aulas/AulaNum01.c
@@ -0,0 +1,12 @@
+// biblioteca de input output
+#include <stdio.h>   
+
+int main(){
+    printf("Mensagem 01\n");
+    printf("Mensagem 02\n");
+    printf("Mensagem 03\n");
+    printf("Mensagem 04\n");
+    printf("Mensagem 05\n");
+    printf("Mensagem 06\n");
+    return 0;
+}
---
+++ Aulas/HelloWorld.c
@@ -0,0 +1,16 @@
+// biblioteca de input output
+#include <stdio.h>   
+
+// biblioteca que permite manipulação da alocação de memória.
+#include <stdlib.h> 
+
+int main(){
+    //imprime a mensagem que estive entre aspas duplas
+    printf("Ola mundo.");
+
+    system("pause");
+
+    //valor de retorno para a função principal
+    //indicando que o programa acabou
+    // return 0;
+}
